f6e68a0f6b66d4ce48dd7f7c93192260f84f8030
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-05-07 23:12:43 +0200

    refactoring

--- src/main/java/org/codecraftlabs/mercury/PemGenerator.java
+++ src/main/java/org/codecraftlabs/mercury/PemGenerator.java
@@ -1,5 +1,6 @@
 package org.codecraftlabs.mercury;
 
+import org.codecraftlabs.mercury.util.AppConstants;
 import org.codecraftlabs.mercury.util.StringUtil;
 
 import java.security.PrivateKey;
@@ -9,33 +10,39 @@ import java.security.cert.CertificateEncodingException;
 import java.util.Base64;
 import java.util.Collection;
 
+import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_CERTIFICATE_KEY;
+import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_PRIVATE_KEY;
+import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_PUBLIC_KEY;
+import static org.codecraftlabs.mercury.util.AppConstants.END_CERTIFICATE_KEY;
+import static org.codecraftlabs.mercury.util.AppConstants.END_PRIVATE_KEY;
+import static org.codecraftlabs.mercury.util.AppConstants.END_PUBLIC_KEY;
 import static org.codecraftlabs.mercury.util.AppConstants.LENGTH;
 
 public class PemGenerator {
     public String export(PublicKey publicKey) {
         String content = Base64.getEncoder().encodeToString(publicKey.getEncoded());
-        StringBuilder formattedContent = new StringBuilder("-----BEGIN PUBLIC KEY-----" + System.lineSeparator());
+        StringBuilder formattedContent = new StringBuilder(BEGIN_PUBLIC_KEY + System.lineSeparator());
         Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
         items.forEach(formattedContent::append);
-        formattedContent.append("-----END PUBLIC KEY-----").append(System.lineSeparator());
+        formattedContent.append(END_PUBLIC_KEY).append(System.lineSeparator());
         return formattedContent.toString();
     }
 
     public String export(PrivateKey key) {
         String content = Base64.getEncoder().encodeToString(key.getEncoded());
-        StringBuilder formattedContent = new StringBuilder("-----BEGIN PRIVATE KEY-----" + System.lineSeparator());
+        StringBuilder formattedContent = new StringBuilder(BEGIN_PRIVATE_KEY + System.lineSeparator());
         Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
         items.forEach(formattedContent::append);
-        formattedContent.append("-----END PRIVATE KEY-----").append(System.lineSeparator());
+        formattedContent.append(END_PRIVATE_KEY).append(System.lineSeparator());
         return formattedContent.toString();
     }
 
     public String export(Certificate certificate) throws CertificateEncodingException {
         String content = Base64.getEncoder().encodeToString(certificate.getEncoded());
-        StringBuilder formattedContent = new StringBuilder("-----BEGIN CERTIFICATE-----" + System.lineSeparator());
+        StringBuilder formattedContent = new StringBuilder(BEGIN_CERTIFICATE_KEY + System.lineSeparator());
         Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
         items.forEach(item -> formattedContent.append(item).append(System.lineSeparator()));
-        formattedContent.append("-----END CERTIFICATE-----").append(System.lineSeparator());
+        formattedContent.append(END_CERTIFICATE_KEY).append(System.lineSeparator());
         return formattedContent.toString();
     }
 }
--- src/main/java/org/codecraftlabs/mercury/util/AppConstants.java
+++ src/main/java/org/codecraftlabs/mercury/util/AppConstants.java
@@ -2,4 +2,10 @@ package org.codecraftlabs.mercury.util;
 
 public class AppConstants {
     public static final int LENGTH = 64;
+    public static final String BEGIN_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----";
+    public static final String END_PUBLIC_KEY = "-----END PUBLIC KEY-----";
+    public static final String BEGIN_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----";
+    public static final String END_PRIVATE_KEY = "-----END PRIVATE KEY-----";
+    public static final String BEGIN_CERTIFICATE_KEY = "-----BEGIN CERTIFICATE KEY-----";
+    public static final String END_CERTIFICATE_KEY = "-----END CERTIFICATE KEY-----";
 }

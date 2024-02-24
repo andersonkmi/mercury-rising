c5b2cda3ce3c2f8c6eb24c1f32a5ae3763307aa8
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-02-24 10:51:55 +0100

    Move method closer to where it's used and remove unused class

--- src/main/java/org/codecraftlabs/mercury/PemGenerator.java
+++ src/main/java/org/codecraftlabs/mercury/PemGenerator.java
@@ -1,15 +1,15 @@
 package org.codecraftlabs.mercury;
 
-import org.codecraftlabs.mercury.util.StringUtil;
-
 import javax.annotation.Nonnull;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collection;
 
+import static java.lang.Math.min;
 import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_CERTIFICATE_KEY;
 import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_PRIVATE_KEY;
 import static org.codecraftlabs.mercury.util.AppConstants.BEGIN_PUBLIC_KEY;
@@ -23,7 +23,7 @@ public class PemGenerator {
     public String export(@Nonnull PublicKey publicKey) {
         StringBuilder formattedContent = new StringBuilder(BEGIN_PUBLIC_KEY + System.lineSeparator());
         String content = encodeToString(publicKey.getEncoded());
-        Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
+        Collection<String> items = splitStringChunks(content, LENGTH);
         items.forEach(formattedContent::append);
         formattedContent.append(END_PUBLIC_KEY).append(System.lineSeparator());
         return formattedContent.toString();
@@ -33,7 +33,7 @@ public class PemGenerator {
     public String export(@Nonnull PrivateKey key) {
         StringBuilder formattedContent = new StringBuilder(BEGIN_PRIVATE_KEY + System.lineSeparator());
         String content = encodeToString(key.getEncoded());
-        Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
+        Collection<String> items = splitStringChunks(content, LENGTH);
         items.forEach(formattedContent::append);
         formattedContent.append(END_PRIVATE_KEY).append(System.lineSeparator());
         return formattedContent.toString();
@@ -43,7 +43,7 @@ public class PemGenerator {
     public String export(@Nonnull Certificate certificate) throws CertificateEncodingException {
         String content = Base64.getEncoder().encodeToString(certificate.getEncoded());
         StringBuilder formattedContent = new StringBuilder(BEGIN_CERTIFICATE_KEY + System.lineSeparator());
-        Collection<String> items = StringUtil.splitStringChunks(content, LENGTH);
+        Collection<String> items = splitStringChunks(content, LENGTH);
         items.forEach(item -> formattedContent.append(item).append(System.lineSeparator()));
         formattedContent.append(END_CERTIFICATE_KEY).append(System.lineSeparator());
         return formattedContent.toString();
@@ -52,5 +52,14 @@ public class PemGenerator {
     @Nonnull
     private String encodeToString(@Nonnull byte[] encodedCertificate) {
         return Base64.getEncoder().encodeToString(encodedCertificate);
+    }
+
+    @Nonnull
+    private Collection<String> splitStringChunks(String str, int size) {
+        ArrayList<String> split = new ArrayList<>();
+        for (int i = 0; i <= str.length() / size; i++) {
+            split.add(str.substring(i * size, min((i + 1) * size, str.length())));
+        }
+        return split;
     }
 }

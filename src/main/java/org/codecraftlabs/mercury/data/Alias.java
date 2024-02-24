c681a2c1c84d59c0d97b3fd07154e8972b34a555
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2024-02-25 00:59:58 +0100

    Convert class to record

--- src/main/java/org/codecraftlabs/mercury/data/Alias.java
+++ src/main/java/org/codecraftlabs/mercury/data/Alias.java
@@ -3,28 +3,7 @@ package org.codecraftlabs.mercury.data;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
 
-public class Alias {
-    final private String name;
-    final private Certificate certificate;
-    final private PrivateKey privateKey;
-
-    public Alias(String name, Certificate certificate, PrivateKey privateKey) {
-        this.name = name;
-        this.certificate = certificate;
-        this.privateKey = privateKey;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Certificate getCertificate() {
-        return certificate;
-    }
-
-    public PrivateKey getPrivateKey() {
-        return privateKey;
-    }
+public record Alias(String name, Certificate certificate, PrivateKey privateKey) {
 
     @Override
     public int hashCode() {
@@ -37,7 +16,7 @@ public class Alias {
             return true;
         }
 
-        if (! getClass().equals(other.getClass())) {
+        if (!getClass().equals(other.getClass())) {
             return false;
         }
 
